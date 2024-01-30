b086da940571ad953c8dd6b6efadad654bf80db6
Damilaretomiwa <faladetomiwa343@gmail.com>
2024-01-30 20:42:03 +0100

    bright push

--- src/App.js
+++ src/App.js
@@ -3,38 +3,26 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomePage from './Components/Welcome/WelcomePage.jsx';
 import VerificationPage from './Components/Verification/VerificationPage.jsx';
 import Register from './Components/Register/Register.jsx';
-
-import PlannerDashboard from '../src/Components/PlannerDashboard/PlannerDashboard.jsx'
-
-
+import PlannerDashboard from '../src/Components/PlannerDashboard/PlannerDashboard.jsx';
 import TalentPortfolio from './Components/TalentProfile/TalentPortfolio';
 import EventDetails from './Components/EventDetails/EventDetails.jsx';
-
+// import Login from './Components/LoginAccount/LoginAccount.jsx';
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<WelcomePage/>}/>
-        <Route path='/verification' element={<VerificationPage/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path={'/Login'} element={<LoginAccount/>}/>
-          <Route path={'/plannerDashboard'} element={<PlannerDashboard/>}/>
-        <Route path='/talentPortfolio' element={<TalentPortfolio/>}/>
-        <Route path='/eventDetails' element={<EventDetails/>}/>
-        {/*<Route path='/Booking' element={<BookingPage/>}/>*/}
-
-      </Routes>
-    </Router>
-  );
-
-
-const AppRouter = () => {
     return (
         <Router>
-            <AppRoutes />
+            <Routes>
+                <Route path='/' element={<WelcomePage />} />
+                <Route path='/verification' element={<VerificationPage />} />
+                <Route path='/register' element={<Register />} />
+                {/*<Route path='/login' element={<LoginAccount />} />*/}
+                <Route path='/plannerDashboard' element={<PlannerDashboard />} />
+                <Route path='/talentPortfolio' element={<TalentPortfolio />} />
+                <Route path='/eventDetails' element={<EventDetails />} />
+                {/*<Route path='/Booking' element={<BookingPage/>}/>*/}
+            </Routes>
         </Router>
     );
 };
 
-export default AppRouter;
+export default App;
