3e56d02489205ed9740bcc951427608f52cfd186
estherpet <estheriyowogoga@gmail.com>
2024-01-31 10:13:04 -0800

    update

--- src/App.js
+++ src/App.js
@@ -5,36 +5,28 @@ import VerificationPage from './Components/Verification/VerificationPage.jsx';
 import Register from './Components/Register/Register.jsx';
 
 import PlannerDashboard from '../src/Components/PlannerDashboard/PlannerDashboard.jsx'
-
-
+import LoginAccount from '../src/Components/LoginAccount/LoginAccount.jsx'
+// import BookingPage from '../src/Components/Bookings/BookingPage.jsx'
 import TalentPortfolio from './Components/TalentProfile/TalentPortfolio';
 import EventDetails from './Components/EventDetails/EventDetails.jsx';
 
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<WelcomePage/>}/>
-        <Route path='/verification' element={<VerificationPage/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path={'/Login'} element={<LoginAccount/>}/>
-          <Route path={'/plannerDashboard'} element={<PlannerDashboard/>}/>
-        <Route path='/talentPortfolio' element={<TalentPortfolio/>}/>
-        <Route path='/eventDetails' element={<EventDetails/>}/>
-        {/*<Route path='/Booking' element={<BookingPage/>}/>*/}
-
-      </Routes>
-    </Router>
-  );
-
-
-const AppRouter = () => {
     return (
         <Router>
-            <AppRoutes />
+            <Routes>
+                <Route path='/' element={<WelcomePage/>}/>
+                <Route path='/verification' element={<VerificationPage/>}/>
+                <Route path='/register' element={<Register/>}/>
+                <Route path={'/Login'} element={<LoginAccount/>}/>
+                <Route path={'/plannerDashboard'} element={<PlannerDashboard/>}/>
+                <Route path='/talentPortfolio' element={<TalentPortfolio/>}/>
+                <Route path='/eventDetails' element={<EventDetails/>}/>
+                <Route path='/Booking' element={<BookingPage/>}/>
+
+            </Routes>
         </Router>
     );
-};
+}
 
-export default AppRouter;
+export default App;
